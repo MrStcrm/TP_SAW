f047e9006f813a0d9516df8b53fc5ecb849cc73c
MrStcrm <163419954+MrStcrm@users.noreply.github.com>
2026-03-25 11:37:40 +0000

    Update Entreprise.java

--- src/main/java/com/ipiecoles/java/java350/model/Entreprise.java
+++ src/main/java/com/ipiecoles/java/java350/model/Entreprise.java
@@ -12,8 +12,8 @@ public final class Entreprise {
     public static final Integer PERFORMANCE_BASE = 1;
     public static final Integer NB_JOURS_MAX_FORFAIT = 218;
     private static final double PRIME_BASE = 1000d;
-
-    public static final Map<NiveauEtude, Double> COEFF_SALAIRE_ETUDES = new EnumMap<>(NiveauEtude.class);
+    
+    protected static final Map<NiveauEtude, Double> COEFF_SALAIRE_ETUDES = new EnumMap<>(NiveauEtude.class);
     private static final Map<Integer, LocalDate> datePaque = new HashMap<>();
 
     private Entreprise() {
